f4046b2c35837c2f3d2edee93a114c5de7889e2b
nilsnolde <nils@openrouteservice.org>
2018-09-29 21:58:49 +0200

    logger set up

---
+++ openelevationservice/server/utils/logger.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+
+def get_logger(name):
+    log_format = '\n%(asctime)s  %(name)8s  %(levelname)5s:  %(message)s'
+    log_path = os.getenv('LOG_PATH', './')
+    logging.basicConfig(format=log_format,
+                        filename=log_path
+                       )
+    console = logging.StreamHandler()
+#    console.setLevel(logging.INFO)
+    console.setFormatter(logging.Formatter(log_format))
+    logging.getLogger(name).addHandler(console)
+    return logging.getLogger(name)
