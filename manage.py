fb70caa58eba47a32261585efaeb42cf4b49a70b
nilsnolde <nils@openrouteservice.org>
2018-09-29 23:48:33 +0200

    load zip in memory instead of disk

--- manage.py
+++ manage.py
@@ -2,14 +2,20 @@
 
 from flask.cli import FlaskGroup
 
-from openelevationservice.server import utils
+from openelevationservice.server.utils import logger
 
 import os
 import logging
 import requests
+import zipfile
 from bs4 import BeautifulSoup
 
-#log = logger(__name__)
+try:
+    from io import BytesIO
+except:
+    from StringIO import StringIO
+
+log = logger.get_logger(__name__)
 
 #app = create_app()
 #cli = FlaskGroup(create_app=create_app)
@@ -18,6 +24,7 @@ from bs4 import BeautifulSoup
 def download():
     """Downloads SRTM tiles to disk"""
     base_url = r'http://data.cgiar-csi.org/srtm/tiles/GeoTIFF/'
+    outdir = os.path.join(os.getcwd(), 'tiles') 
     
     # Create session for authentication
     session = requests.Session()
@@ -26,8 +33,21 @@ def download():
     
     soup = BeautifulSoup(response.content)
     
+    # First find all 'a' tags starting href with srtm*
     for link in soup.find_all('a', attrs={'href': lambda x: x.startswith('srtm')}):
-        print(link.text)
+        # Then load the zip data in memory
+        with zipfile.ZipFile(BytesIO(session.get(base_url + link.text).content)) as zip_obj:
+            # Loop through the files in the zip
+            for filename in zip_obj.namelist():
+                # Don't extract the readme.txt
+                if filename != 'readme.txt':
+                    data = zip_obj.read(filename)
+                    # Write byte contents to file
+                    with open(os.path.join(os.getcwd(), filename), 'wb') as f:
+                        f.write(data)
+        
+        log.debug("Downloaded file {}".format(link.text))
+        
  
 if __name__ == "__main__":
     download()
