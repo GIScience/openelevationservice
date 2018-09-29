86365dce49cc1192926f2f22a67b71b9c5c33822
nilsnolde <nils@openrouteservice.org>
2018-09-29 23:20:19 +0200

    no file logging

--- openelevationservice/server/utils/logger.py
+++ openelevationservice/server/utils/logger.py
@@ -7,7 +7,6 @@ def get_logger(name):
     log_format = '\n%(asctime)s  %(name)8s  %(levelname)5s:  %(message)s'
     log_path = os.getenv('LOG_PATH', './')
     logging.basicConfig(format=log_format,
-                        filename=log_path
                        )
     console = logging.StreamHandler()
 #    console.setLevel(logging.INFO)
