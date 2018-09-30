c08e1d837629407c730b713dabf37b63097593eb
nilsnolde <nils@openrouteservice.org>
2018-09-30 13:19:40 +0200

    break after 50/872 files for dev

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 from flask.cli import FlaskGroup
 
 from openelevationservice.server.utils import logger
+from openelevationservice.server import SETTINGS
 
 import os
 import logging
@@ -28,23 +29,28 @@ def download():
     
     # Create session for authentication
     session = requests.Session()
-    session.auth = ('data_public', 'GDdci')
+    session.auth = tuple(SETTINGS['srtm_parameters'].values())
     response = session.get(base_url)
     
     soup = BeautifulSoup(response.content)
     
     # First find all 'a' tags starting href with srtm*
+    counter = 0
     for link in soup.find_all('a', attrs={'href': lambda x: x.startswith('srtm')}):
-        # Then load the zip data in memory
-        with zipfile.ZipFile(BytesIO(session.get(base_url + link.text).content)) as zip_obj:
-            # Loop through the files in the zip
-            for filename in zip_obj.namelist():
-                # Don't extract the readme.txt
-                if filename != 'readme.txt':
-                    data = zip_obj.read(filename)
-                    # Write byte contents to file
-                    with open(os.path.join(os.getcwd(), filename), 'wb') as f:
-                        f.write(data)
+        counter += 1
+        if counter == 450:
+            break
+        else:
+            # Then load the zip data in memory
+            with zipfile.ZipFile(BytesIO(session.get(base_url + link.text).content)) as zip_obj:
+                # Loop through the files in the zip
+                for filename in zip_obj.namelist():
+                    # Don't extract the readme.txt
+                    if filename != 'readme.txt':
+                        data = zip_obj.read(filename)
+                        # Write byte contents to file
+                        with open(os.path.join(outdir, filename), 'wb') as f:
+                            f.write(data)
         
         log.debug("Downloaded file {}".format(link.text))
         
