b6eb8474928c6286cd50299fd614dd9fb3b9b37e
nilsnolde <nils@openrouteservice.org>
2018-09-30 13:35:47 +0200

    add log file

--- openelevationservice/server/utils/logger.py
+++ openelevationservice/server/utils/logger.py
@@ -7,9 +7,11 @@ def get_logger(name):
     log_format = '\n%(asctime)s  %(name)8s  %(levelname)5s:  %(message)s'
     log_path = os.getenv('LOG_PATH', './')
     logging.basicConfig(format=log_format,
+                        filename=os.path.join(os.getcwd(), 'oes_dev_log.log'),
+                        filemode='w'
                        )
     console = logging.StreamHandler()
-#    console.setLevel(logging.INFO)
+    console.setLevel(logging.DEBUG)
     console.setFormatter(logging.Formatter(log_format))
     logging.getLogger(name).addHandler(console)
     return logging.getLogger(name)
