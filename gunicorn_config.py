264da37e0a29a431bd78f4b82edad99aa4b3d6c3
nilsnolde <nilsnolde@gmail.com>
2018-10-21 17:41:15 +0200

    gunicorn config file

---
+++ gunicorn_config.py
@@ -0,0 +1,7 @@
+bind = "0.0.0.0:3135"
+# Generally we recommend (2 x $num_cores) + 1 as the number of workers to start off with
+workers = 4
+worker_class = 'gevent'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
