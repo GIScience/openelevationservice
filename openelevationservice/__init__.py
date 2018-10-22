fb913739b06e61b84bc3b87297fb81dcd4ab8bf6
nilsnolde <nilsnolde@gmail.com>
2018-10-22 22:41:48 +0200

    checking for TILES_DIR and create if not exists

--- openelevationservice/__init__.py
+++ openelevationservice/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from os import getcwd, path, environ
+from os import getcwd, path, environ, makedirs
 from yaml import safe_load
 
 basedir = path.abspath(path.dirname(__file__))
@@ -9,5 +9,7 @@ if "TESTING" in environ:
     SETTINGS['provider_parameters']['table_name'] = SETTINGS['provider_parameters']['table_name'] + '_test'
 
 TILES_DIR = path.join(getcwd(), 'tiles')
+if not path.exists(TILES_DIR):
+    makedirs(TILES_DIR)
 
 __version__ = "0.1"
