1c7b327b9fd5398a773f3eca88f60e0ffd6c03ee
nilsnolde <nilsnolde@gmail.com>
2019-03-25 11:00:23 +0300

    encodedpolyline was confusing x,y.. damn. convention is lat, long, not x, y.

--- openelevationservice/server/utils/codec.py
+++ openelevationservice/server/utils/codec.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from .convert import polyline_to_geometry
+
 import itertools
 import six
 import math
@@ -48,7 +50,7 @@ def _trans(value, index):
     return ~(result >> 1) if comp else (result >> 1), index
 
 
-def decode(expression, precision=5, is3D=False):
+def decode(expression, precision=5, is3d=False):
     coordinates, index, lat, lng, z, length, factor = [], 0, 0, 0, 0, len(expression), float(10 ** precision)
 
     while index < length:
@@ -56,28 +58,31 @@ def decode(expression, precision=5, is3D=False):
         lng_change, index = _trans(expression, index)
         lat += lat_change
         lng += lng_change
-        if not is3D:
-            coordinates.append((lat / factor, lng / factor))
+        # Careful, Lng/Lat are interchanged here, so that shapely converts correctly
+        if not is3d:
+            coordinates.append((lng / factor, lat / factor))
         else:
             z_change, index = _trans(expression, index)
             z += z_change
-            coordinates.append((lat / factor, lng / factor, z / 100))
+            coordinates.append((lng / factor, lat / factor, z / 100))
 
-    return coordinates
+    return polyline_to_geometry(coordinates)
 
 
-def encode(coordinates, precision=5, is3D=False):
+def encode(coordinates, precision=5, is3d=False):
     output, factor = six.StringIO(), int(10 ** precision)
 
-    _write(output, coordinates[0][0], 0, factor)
+    # Careful, Lnt/Lat are interchanged here, bcs PostGIS query outputs x, y, but encodedpolyline has the convention of
+    # lat, lon
     _write(output, coordinates[0][1], 0, factor)
-    if is3D:
+    _write(output, coordinates[0][0], 0, factor)
+    if is3d:
         _write(output, coordinates[0][2], 0, 100)
 
     for prev, curr in _pcitr(coordinates):
-        _write(output, curr[0], prev[0], factor)
         _write(output, curr[1], prev[1], factor)
-        if is3D==True:
+        _write(output, curr[0], prev[0], factor)
+        if is3d==True:
             _write(output, curr[2], prev[2], 100)
 
     return output.getvalue()
