61ace279648b362279be2f12695af5b8f3965be3
liu-hanli <hanli.liu@edu.uah.es>
2023-03-10 17:58:30 +0100

    Area query with some points improperly excluded

--- openelevationservice/server/api/querybuilder.py
+++ openelevationservice/server/api/querybuilder.py
@@ -3,11 +3,11 @@
 from openelevationservice import SETTINGS
 from openelevationservice.server.utils.logger import get_logger
 from openelevationservice.server.db_import.models import db, Cgiar
-from openelevationservice.server.utils.custom_func import ST_SnapToGrid
+# from openelevationservice.server.utils.custom_func import ST_SnapToGrid
 from openelevationservice.server.api.api_exceptions import InvalidUsage
 
-from geoalchemy2.functions import ST_DumpPoints, ST_Dump, ST_Value, ST_Intersects, ST_X, ST_Y
-from sqlalchemy import func
+from geoalchemy2.functions import ST_Value, ST_Intersects, ST_X, ST_Y # ST_DumpPoints, ST_Dump, 
+from sqlalchemy import func, literal_column
 import json
 
 log = get_logger(__name__)
@@ -17,6 +17,8 @@ if "/" in coord_precision:
     coord_precision = float(coord_precision.split("/")[0]) / float(coord_precision.split("/")[1])
 else:
     coord_precision = float(coord_precision)
+
+division_limit = 1 / float(SETTINGS['maximum_nodes'])
 
 def _getModel(dataset):
     """
@@ -32,6 +34,21 @@ def _getModel(dataset):
         model = Cgiar
     
     return model
+
+
+def format_PixelAsPoints(result_pixels):
+    # format: [ ('(0101000020E61000000000000000C05240D169039D36003D40,202,1,2)',) , ...
+    points = []
+    heights = []
+    for pixel in result_pixels:
+        subcolumns = pixel[0].split(",")
+        point = subcolumns[0][1: ]
+        if point not in points:
+            points.append(point)
+            heights.append(int(subcolumns[1]))
+    
+    return func.unnest(literal_column("ARRAY{}".format(points))), \
+           func.unnest(literal_column("ARRAY{}".format(heights)))
 
 
 def polygon_elevation(geometry, format_out, dataset):
@@ -56,33 +73,43 @@ def polygon_elevation(geometry, format_out, dataset):
     Model = _getModel(dataset)
     
     if geometry.geom_type == 'Polygon':
-        query_points2d = db.session\
-                            .query(func.ST_SetSRID(ST_DumpPoints(geometry.wkt).geom, 4326) \
+        query_geom = db.session \
+                            .query(func.ST_SetSRID(func.ST_PolygonFromText(geometry.wkt), 4326) \
                             .label('geom')) \
-                            .subquery().alias('points2d')
+                            .subquery().alias('pGeom')
 
-        query_getelev = db.session \
-                            .query(query_points2d.c.geom,
-                                   ST_Value(Model.rast, query_points2d.c.geom).label('z')) \
-                            .filter(ST_Intersects(Model.rast, query_points2d.c.geom)) \
-                            .subquery().alias('getelevation')
+        result_pixels = db.session \
+                            .query(func.ST_PixelAsPoints(
+                                func.ST_Clip(Model.rast, 1, query_geom.c.geom, coord_precision), #.geom
+                                1)) \
+                            .select_from(query_geom.join(Model, ST_Intersects(Model.rast, query_geom.c.geom))) \
+                            .all() #.subquery().alias('getsubrast')
+        
+        point_col, height_col = format_PixelAsPoints(result_pixels)
+        
+        #
+        #query_getelev = db.session \
+        #                    .query(query_subrast.c.geom,
+        #                           query_subrast.c.val) \
+        #                    .subquery().alias('getelev')
 
         query_points3d = db.session \
-                            .query(func.ST_SetSRID(func.ST_MakePoint(ST_X(query_getelev.c.geom),
-                                                                     ST_Y(query_getelev.c.geom),
-                                                                     query_getelev.c.z),
+                            .query(func.ST_SetSRID(func.ST_MakePoint(ST_X(point_col),
+                                                                     ST_Y(point_col),
+                                                                     height_col),
                                               4326).label('geom')) \
+                            .order_by(ST_X(point_col), ST_Y(point_col)) \
                             .subquery().alias('points3d')
 
         if format_out == 'geojson':
             # Return GeoJSON directly in PostGIS
             query_final = db.session \
-                              .query(func.ST_AsGeoJson(func.ST_MakePolygon(ST_SnapToGrid(query_points3d.c.geom, coord_precision))))
+                              .query(func.ST_AsGeoJson(func.ST_MakeLine(query_points3d.c.geom)))
             
         else:
             # Else return the WKT of the geometry
             query_final = db.session \
-                              .query(func.ST_AsText(func.ST_MakePolygon(ST_SnapToGrid(query_points3d.c.geom, coord_precision))))
+                              .query(func.ST_AsText(func.ST_MakeLine(query_points3d.c.geom)))
     else:
         raise InvalidUsage(400, 4002, "Needs to be a Polygon, not a {}!".format(geometry.geom_type))
 
@@ -131,7 +158,7 @@ def line_elevation(geometry, format_out, dataset):
                                 func.ST_PointN(geometry.wkt, 1),
                                 func.ST_LineInterpolatePoints(
                                     geometry.wkt,
-                                    min(1, coord_precision / lineLen)
+                                    max(min(1, coord_precision / lineLen), division_limit)
                                 )
                             )).geom, 4326).label('geom')).subquery().alias('points2d')
         
@@ -152,6 +179,7 @@ def line_elevation(geometry, format_out, dataset):
                                                                      ST_Y(query_getelev.c.geom),
                                                                      query_getelev.c.z),
                                               4326).label('geom')) \
+                            .order_by(ST_X(query_getelev.c.geom)) \
                             .subquery().alias('points3d')
 
         if format_out == 'geojson':
@@ -209,16 +237,16 @@ def point_elevation(geometry, format_out, dataset):
         
         if format_out == 'geojson': 
             query_final = db.session \
-                                .query(func.ST_AsGeoJSON(ST_SnapToGrid(func.ST_MakePoint(ST_X(query_getelev.c.geom),
-                                                                                           ST_Y(query_getelev.c.geom),
-                                                                                           query_getelev.c.z),
-                                                                        coord_precision)))
+                                .query(func.ST_AsGeoJSON(func.ST_MakePoint(ST_X(query_getelev.c.geom),
+                                                                                         ST_Y(query_getelev.c.geom),
+                                                                                         query_getelev.c.z)
+                                                                        ))
         else:
             query_final = db.session \
-                                .query(func.ST_AsText(ST_SnapToGrid(func.ST_MakePoint(ST_X(query_getelev.c.geom),
-                                                                                       ST_Y(query_getelev.c.geom),
-                                                                                       query_getelev.c.z),
-                                                                    coord_precision)))
+                                .query(func.ST_AsText(func.ST_MakePoint(ST_X(query_getelev.c.geom),
+                                                                        ST_Y(query_getelev.c.geom),
+                                                                        query_getelev.c.z)
+                                                                    ))
     else:
         raise InvalidUsage(400, 4002, "Needs to be a Point, not {}!".format(geometry.geom_type))
     
